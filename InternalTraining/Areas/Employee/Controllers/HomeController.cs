a9f1ccbc84ce349bfaff36f4bae502891c1c0071
Sarah Mustafa <sarah.taye99@gmail.com>
2025-05-01 21:48:10 +0300

    Employee Controller

--- InternalTraining/Areas/Employee/Controllers/HomeController.cs
+++ InternalTraining/Areas/Employee/Controllers/HomeController.cs
@@ -1,12 +1,188 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InternalTraining.Models;
+using InternalTraining.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 
 namespace InternalTraining.Areas.Employee.Controllers
 {
-    public class HomeController : Controller
+    [Area("Employee")]
+    public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
-            return View();
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this._roleManager = roleManager;
         }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            if (_roleManager.Roles.IsNullOrEmpty())
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                await _roleManager.CreateAsync(new IdentityRole("Company"));
+                await _roleManager.CreateAsync(new IdentityRole("Employee"));
+            }
+            return View(new RegisterVm());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVm registerVm)
+        {
+
+            if (ModelState.IsValid)
+            {
+                ApplicationUser appUser = new ApplicationUser
+                {
+                    UserName = registerVm.UserName,
+                    Email = registerVm.Email,
+                };
+
+                var result = await userManager.CreateAsync(appUser, registerVm.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(appUser, false);
+                    return RedirectToAction("Index", "Home", new { area = "Employee" });
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+
+            }
+            return View(registerVm);
+        }
+
+        public IActionResult Login()
+        {
+            return View(new LoginVm());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVm loginVm)
+        {
+            if (ModelState.IsValid)
+            {
+                var appUser = await userManager.FindByEmailAsync(loginVm.Email);
+
+                if (appUser != null)
+                {
+                    var result = await userManager.CheckPasswordAsync(appUser, loginVm.Password);
+                    if (result)
+                    {
+                        await signInManager.SignInAsync(appUser, loginVm.RememberMe);
+                        return RedirectToAction("Index", "Home", new { area = "Employee" });
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Email", "Cannot Found The Email");
+                        ModelState.AddModelError("Password", "Donot Match The Password");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("Email", "Cannot Found The Email");
+                    ModelState.AddModelError("Password", "Donot Match The Password");
+
+                }
+            }
+            return View(loginVm);
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            var appUser = await userManager.GetUserAsync(User);
+            if (appUser == null)
+            {
+                ModelState.AddModelError("", "Sorry Something is Wrong");
+            }
+
+            var profileInfo = new ProfileVm
+            {
+                Id = appUser.Id,
+                UserName = appUser.UserName,
+                Email = appUser.Email
+            };
+            return View(profileInfo);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Profile(ProfileVm profileVm)
+        {
+            if (ModelState.IsValid)
+            {
+                var appUser = await userManager.GetUserAsync(User);
+                if (appUser == null)
+                {
+                    ModelState.AddModelError("", "Sorry Something is wrong");
+                    return View(profileVm);
+                }
+
+                if (appUser.Email != profileVm.Email)
+                {
+                    var emailToken = await userManager.GenerateChangeEmailTokenAsync(appUser, profileVm.Email);
+                    var result = await userManager.ChangeEmailAsync(appUser, profileVm.Email, emailToken);
+
+                    if (!result.Succeeded)
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                        return View(profileVm);
+                    }
+
+                }
+
+                if (appUser.UserName != profileVm.UserName)
+                {
+                    appUser.UserName = profileVm.UserName;
+                    var result = await userManager.UpdateAsync(appUser);
+                    if (!result.Succeeded)
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                        return View(profileVm);
+                    }
+
+                }
+
+                if (!string.IsNullOrEmpty(profileVm.CurrentPassword) && !string.IsNullOrEmpty(profileVm.NewPassword))
+                {
+                    var result = await userManager.ChangePasswordAsync(appUser, profileVm.CurrentPassword, profileVm.NewPassword);
+                    if (!result.Succeeded)
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                        return View(profileVm);
+                    }
+
+                }
+                await signInManager.RefreshSignInAsync(appUser);
+                return RedirectToAction("Index", "Home", new { area = "Customer" });
+            }
+            return View(profileVm);
+        }
+
     }
 }
